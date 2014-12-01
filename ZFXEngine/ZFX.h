085d45805e1c7df65a698a236076e4b23dd760a4
Chris Cheng <gemini2015@hotmail.com>
2014-12-01 15:08:55 +0800

    增加Skin Manager

--- ZFXEngine/ZFX.h
+++ ZFXEngine/ZFX.h
@@ -1,3 +1,5 @@
+#ifndef _ZFX_H_
+#define _ZFX_H_
 
 // everything went smooth
 #define ZFX_OK                        S_OK
@@ -11,16 +13,60 @@
 // specific error messages
 #define ZFX_CREATEAPI                 0x82000002
 #define ZFX_CREATEDEVICE              0x82000003
-#define ZFX_INVALIDPARAM              0x82000004
-#define ZFX_NODEPTHSTENCIL            0x82000005
-#define ZFX_FAIL5               0x82000006
-#define ZFX_FAIL6               0x82000007
-#define ZFX_FAIL7               0x82000008
-#define ZFX_FAIL8               0x82000009
-#define ZFX_FAIL9               0x8200000a
-#define ZFX_FAIL0               0x8200000b
+#define ZFX_CREATEBUFFER              0x82000004
+#define ZFX_INVALIDPARAM              0x82000005
+#define ZFX_INVALIDID                 0x82000006
+#define ZFX_BUFFERSIZE                0x82000007
+#define ZFX_BUFFERLOCK                0x82000008
+#define ZFX_NOTCOMPATIBLE             0x82000009
+#define ZFX_OUTOFMEMORY               0x8200000a
+#define ZFX_FILENOTFOUND              0x8200000b
+#define ZFX_INVALIDFILE               0x8200000c
+#define ZFX_NOSHADERSUPPORT           0x8200000d
 #define ZFX_FAILa               0x8200000c
 #define ZFX_FAILb               0x8200000d
 #define ZFX_FAILc               0x8200000e
 #define ZFX_FAILd               0x8200000f
 
+struct _ZFXCOLOR
+{
+	union
+	{
+		struct  
+		{
+			float fR;
+			float fG;
+			float fB;
+			float fA;
+		};
+		float c[4];
+	};
+};
+typedef _ZFXCOLOR ZFXCOLOR;
+
+typedef struct _ZFXMaterial
+{
+	ZFXCOLOR cDiffuse;
+	ZFXCOLOR cAmbient;
+	ZFXCOLOR cSpecular;
+	ZFXCOLOR cEmissive;
+	float fPower;
+}ZFXMATERIAL;
+
+typedef struct _ZFXTexture
+{
+	float fAlpha;
+	char *chName;
+	void *pData;
+	ZFXCOLOR *pClrKeys;
+	DWORD dwNum;
+}ZFXTEXTURE;
+
+typedef struct _ZFXSkin
+{
+	bool bAlpha;
+	UINT nMaterial;
+	UINT nTexture[8];
+}ZFXSKIN;
+
+#endif
--- ZFXRenderer/ZFXRenderDevice.h
+++ ZFXRenderer/ZFXRenderDevice.h
@@ -5,17 +5,58 @@
 
 // I N C L U D E S /////////////////////////////////////////////////
 
-#include <windows.h>
-#include <stdio.h>
+#include <windows.h>          // windows types and stuff
+#include <stdio.h>            // file stuff
+#include "zfx.h"       // MATERIAL structure
+#include "zfx3d.h"            // zfx 3d math library
 
 
 // D E F I N E S ///////////////////////////////////////////////////
 
-#define MAX_3DHWND 8
-
 
 
 // S T R U C T S ///////////////////////////////////////////////////
+
+/**
+ * ZFXMaterialManager is an abstract class which provides an interface
+ * from which material managers can inherit. Those heirs need to 
+ * implement a store for texture data in a way appropriate to the
+ * API.
+ */
+class ZFXSkinManager {
+   protected:
+      UINT         m_nNumSkins;           // number (IDs) of skins
+      UINT         m_nNumMaterials;       // number (IDs) of materials
+      UINT         m_nNumTextures;        // number (IDs) of materials
+      ZFXSKIN     *m_pSkins;              // allocate mem for skins here
+      ZFXMATERIAL *m_pMaterials;          // allocate mem for materials here
+      ZFXTEXTURE  *m_pTextures;           // allocate mem for textures here
+
+
+   public:
+      ZFXSkinManager(void) {};
+      virtual ~ZFXSkinManager(void) {};
+
+      virtual HRESULT AddSkin(const ZFXCOLOR *pcAmbient,
+                              const ZFXCOLOR *pcDiffuse,
+                              const ZFXCOLOR *pcEmissive,
+                              const ZFXCOLOR *pcSpecular,
+                              float fSpecPower,
+                              UINT *nSkinID)=0;
+
+      virtual HRESULT AddTexture(UINT nSkinID, const char *chName,
+                                 bool bAlpha, float fAlpha,
+                                 ZFXCOLOR *cColorKeys,
+                                 DWORD dwNumColorKeys)=0;
+
+      virtual bool    MaterialEqual(const ZFXMATERIAL *pMat0, 
+                                    const ZFXMATERIAL *pMat1)=0;
+
+      virtual void    LogCurrentStatus(char *chLog, 
+                                       bool bDetailed)=0;
+   };
+/*----------------------------------------------------------------*/
+
 
 /**
  * ZFXRenderDevice is an abstract class which provides an interface
@@ -24,31 +65,35 @@
  */
 class ZFXRenderDevice {
    protected:
-      HWND           m_hWndMain;          // application main window
-      HWND           m_hWnd[MAX_3DHWND];  // 3D render window handle
-      UINT           m_nNumhWnd;          // number of hwnds in array
-      UINT           m_nActivehWnd;       // which one is active (0=main hWnd)
-      HINSTANCE      m_hDLL;              // dll module handle
-      DWORD          m_dwWidth;           // screen width
-      DWORD          m_dwHeight;          // screen height
-      bool           m_bWindowed;         // windowed mode?
-      char           m_chAdapter[256];    // graphics adapter name
-      bool           m_bRunning;          // after succesful initialization
-      FILE          *m_pLog;              // log file
-      
+      HWND      m_hWnd;             // 3D render window handle
+      HINSTANCE m_hDLL;             // dll module handle
+      DWORD     m_dwWidth;          // screen width
+      DWORD     m_dwHeight;         // screen height
+      bool      m_bWindowed;        // windowed mode?
+      char      m_chAdapter[256];   // graphics adapter name
+      bool      m_bRunning;         // after succesful initialization
+      FILE     *m_pLog;             // log file
+
+      float     m_fNear,            // near clipping plane
+                m_fFar;             // far clipping plane
+      int       m_nMode;            // 0=perspective, 1=ortho
+      int       m_nStage;           // stage (0-3) for viewport/projection
+      RECT      m_rcVP[4];          // viewports for all 4 stages
+
       
    public:
+      ZFXSkinManager *m_pSkinMan;   // material and textures
+
       ZFXRenderDevice(void) {};
       virtual ~ZFXRenderDevice(void) {};
       
-      
+
       // INIT/RELEASE STUFF:
       // ===================
       
       // initialize engine, should first display dialog to select
       // graphics adapter and mode, and then start the 3D API
-      virtual HRESULT Init(HWND, const HWND*, int, int, int, bool)=0;
-      virtual HRESULT InitWindowed(HWND, const HWND*, int, bool)=0;
+      virtual HRESULT Init(HWND, HWND, int, int, bool)=0;
       
       // release API specific stuff
       virtual void Release(void)=0;
@@ -57,12 +102,43 @@ class ZFXRenderDevice {
       virtual bool IsRunning(void)=0;
       
       
+
+      // MISC OPERATIONS:
+      // ================
+
+      // set view matrix from cam's vRight, vUp, vDir, vPos
+      virtual HRESULT SetView3D(const ZFXVector&, const ZFXVector&, 
+                                const ZFXVector&, const ZFXVector&)=0;
+
+      // set look at matrix: from, at, world up
+      virtual HRESULT SetViewLookAt(const ZFXVector&, const ZFXVector&, 
+                                    const ZFXVector&)=0;
+
+      // set engines near and far clipping plane
+      virtual void    SetClippingPlanes(float, float)=0;
+
+      // set mode for stage n, 0:=3D(perspective), 1:=2D(orthogonal)
+      virtual HRESULT SetMode(int, int n)=0;
+
+      // set perspective FOV and viewport for stage n
+      virtual HRESULT SetFOV(float, RECT*, int n)=0;
+
+      // get frustrum planes
+      virtual HRESULT GetFrustrum(ZFXPlane*)=0;
+
+      // screen to worldray, give 2 vectors for output
+      virtual void Transform2Dto3D(const POINT &pt, 
+                                   ZFXVector *vcO, 
+                                   ZFXVector *vcD)=0;
+
+      // cast world position to screen coordinates
+      virtual POINT Transform3Dto2D(const ZFXVector &vcP)=0;
+
+
+
       // RENDERING STUFF:
       // ================
       
-      // switch swap chain to hwnd in array from Init()
-      virtual HRESULT UseWindow(UINT nHwnd)=0;
-
       // clear buffers and prepare for rendering
       virtual HRESULT BeginRendering(bool bClearPixel, 
                                      bool bClearDepth, 
@@ -76,6 +152,10 @@ class ZFXRenderDevice {
       
       // change background color
       virtual void SetClearColor(float fRed, float fGreen, float fBlue)=0;
+
+      // Fade screen to given color with given intensity
+      virtual void FadeScreen(float fR, float fG, float fB, float fA)=0;
+
    }; // class
 
 typedef class ZFXRenderDevice *LPZFXRENDERDEVICE;
@@ -83,10 +163,10 @@ typedef class ZFXRenderDevice *LPZFXRENDERDEVICE;
 
 
 extern "C" {
-   //HRESULT CreateRenderDevice(HINSTANCE hDLL, ZFXRenderDevice **pInterface);
+   HRESULT CreateRenderDevice(HINSTANCE hDLL, ZFXRenderDevice **pInterface);
    typedef HRESULT (*CREATERENDERDEVICE)(HINSTANCE hDLL, ZFXRenderDevice **pInterface);
    
-   //HRESULT ReleaseRenderDevice(ZFXRenderDevice **pInterface);
+   HRESULT ReleaseRenderDevice(ZFXRenderDevice **pInterface);
    typedef HRESULT (*RELEASERENDERDEVICE)(ZFXRenderDevice **pInterface);
    }
 /*----------------------------------------------------------------*/

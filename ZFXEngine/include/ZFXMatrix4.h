de44fd345ccb225d23c8973a6108e4f6846106cb
Chris Cheng <gemini2015@hotmail.com>
2015-03-31 14:33:59 +0800

    add matrix 3

---
+++ ZFXEngine/include/ZFXMatrix4.h
@@ -0,0 +1,341 @@
+#ifndef _ZFX_MATRIX_4_H_
+#define _ZFX_MATRIX_4_H_
+
+#include "ZFXPrerequisites.h"
+#include "ZFXVector4.h"
+#include "ZFXPlane.h"
+#include "ZFXMatrix3.h"
+
+namespace ZFX
+{
+	class Matrix4
+	{
+	public:
+		union
+		{
+			float32 val[16];
+			float32 m[4][4];
+		};
+
+	public:
+		inline Matrix4() {}
+
+		inline Matrix4(
+			float f11, float f12, float f13, float f14,
+			float f21, float f22, float f23, float f24,
+			float f31, float f32, float f33, float f34,
+			float f41, float f42, float f43, float f44
+			)
+		{
+			m[0][0] = f11;
+			m[0][1] = f12;
+			m[0][2] = f13;
+			m[0][3] = f14;
+
+			m[1][0] = f21;
+			m[1][1] = f22;
+			m[1][2] = f23;
+			m[1][3] = f24;
+
+			m[2][0] = f31;
+			m[2][1] = f32;
+			m[2][2] = f33;
+			m[2][3] = f34;
+
+			m[3][0] = f41;
+			m[3][1] = f42;
+			m[3][2] = f43;
+			m[3][3] = f44;
+		}
+
+		inline Matrix4(const Matrix3& mat)
+		{
+			operator=(IDENTITY);
+			operator=(mat);
+		}
+
+		/*inline Matrix4(const Quaternion& rot)
+		{
+		Matrix3 m3x3;
+		rot.ToRotationMatrix(m3x3);
+		operator=(IDENTITY);
+		operator=(m3x3);
+		}*/
+
+		inline void Identity()
+		{
+			for (size_t i = 0; i < 16; i++)
+				val[i] = 0.0f;
+			m[0][0] = 1.0f;
+			m[1][1] = 1.0f;
+			m[2][2] = 1.0f;
+			m[3][3] = 1.0f;
+		}
+
+		inline void Swap(Matrix4& mat)
+		{
+			for (size_t i = 0; i < 16; i++)
+				std::swap(val[i], mat.val[i]);
+		}
+
+		inline float32* operator[](size_t row)
+		{
+			assert(row < 4);
+			return m[row];
+		}
+
+		inline const float32* operator[](size_t row) const
+		{
+			assert(row < 4);
+			return m[row];
+		}
+
+		inline Matrix4 operator*(const Matrix4& mat) const
+		{
+			Matrix4 ret;
+			ret.m[0][0] = m[0][0] * mat.m[0][0] + m[0][1] * mat.m[1][0] + m[0][2] * mat.m[2][0] + m[0][3] * mat.m[3][0];
+			ret.m[0][1] = m[0][0] * mat.m[0][1] + m[0][1] * mat.m[1][1] + m[0][2] * mat.m[2][1] + m[0][3] * mat.m[3][1];
+			ret.m[0][2] = m[0][0] * mat.m[0][2] + m[0][1] * mat.m[1][2] + m[0][2] * mat.m[2][2] + m[0][3] * mat.m[3][2];
+			ret.m[0][3] = m[0][0] * mat.m[0][3] + m[0][1] * mat.m[1][3] + m[0][2] * mat.m[2][3] + m[0][3] * mat.m[3][3];
+
+			ret.m[1][0] = m[1][0] * mat.m[0][0] + m[1][1] * mat.m[1][0] + m[1][2] * mat.m[2][0] + m[1][3] * mat.m[3][0];
+			ret.m[1][1] = m[1][0] * mat.m[0][1] + m[1][1] * mat.m[1][1] + m[1][2] * mat.m[2][1] + m[1][3] * mat.m[3][1];
+			ret.m[1][2] = m[1][0] * mat.m[0][2] + m[1][1] * mat.m[1][2] + m[1][2] * mat.m[2][2] + m[1][3] * mat.m[3][2];
+			ret.m[1][3] = m[1][0] * mat.m[0][3] + m[1][1] * mat.m[1][3] + m[1][2] * mat.m[2][3] + m[1][3] * mat.m[3][3];
+
+			ret.m[2][0] = m[2][0] * mat.m[0][0] + m[2][1] * mat.m[1][0] + m[2][2] * mat.m[2][0] + m[2][3] * mat.m[3][0];
+			ret.m[2][1] = m[2][0] * mat.m[0][1] + m[2][1] * mat.m[1][1] + m[2][2] * mat.m[2][1] + m[2][3] * mat.m[3][1];
+			ret.m[2][2] = m[2][0] * mat.m[0][2] + m[2][1] * mat.m[1][2] + m[2][2] * mat.m[2][2] + m[2][3] * mat.m[3][2];
+			ret.m[2][3] = m[2][0] * mat.m[0][3] + m[2][1] * mat.m[1][3] + m[2][2] * mat.m[2][3] + m[2][3] * mat.m[3][3];
+
+			ret.m[3][0] = m[3][0] * mat.m[0][0] + m[3][1] * mat.m[1][0] + m[3][2] * mat.m[2][0] + m[3][3] * mat.m[3][0];
+			ret.m[3][1] = m[3][0] * mat.m[0][1] + m[3][1] * mat.m[1][1] + m[3][2] * mat.m[2][1] + m[3][3] * mat.m[3][1];
+			ret.m[3][2] = m[3][0] * mat.m[0][2] + m[3][1] * mat.m[1][2] + m[3][2] * mat.m[2][2] + m[3][3] * mat.m[3][2];
+			ret.m[3][3] = m[3][0] * mat.m[0][3] + m[3][1] * mat.m[1][3] + m[3][2] * mat.m[2][3] + m[3][3] * mat.m[3][3];
+
+			return ret;
+		}
+
+		inline Vector4 operator * (const Vector4& v) const
+		{
+			return Vector4(
+				m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z + m[0][3] * v.w,
+				m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z + m[1][3] * v.w,
+				m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z + m[2][3] * v.w,
+				m[3][0] * v.x + m[3][1] * v.y + m[3][2] * v.z + m[3][3] * v.w
+				);
+		}
+
+		inline Plane operator * (const Plane& p) const
+		{
+			Plane ret;
+			Matrix4 invTrans = Inverse().Transpose();
+			Vector4 v4(p.normal.x, p.normal.y, p.normal.z, p.d);
+			v4 = invTrans * v4;
+			ret.normal.x = v4.x;
+			ret.normal.y = v4.y;
+			ret.normal.z = v4.z;
+			ret.d = (v4.w) / ret.normal.GetSqrLength();
+
+			return ret;
+		}
+
+		inline Matrix4 operator*(float32 scalar) const
+		{
+			Matrix4 ret;
+			for (size_t i = 0; i < 16; i++)
+			{
+				ret.val[i] = val[i] * scalar;
+			}
+			return ret;
+		}
+
+		inline Matrix4 operator+(const Matrix4& mat) const
+		{
+			Matrix4 ret;
+			for (size_t i = 0; i < 16; i++)
+			{
+				ret.val[i] = val[i] + mat.val[i];
+			}
+			return ret;
+		}
+
+		inline Matrix4 operator-(const Matrix4& mat) const
+		{
+			Matrix4 ret;
+			for (size_t i = 0; i < 16; i++)
+			{
+				ret.val[i] = val[i] - mat.val[i];
+			}
+			return ret;
+		}
+
+		inline bool operator==(const Matrix4& mat) const
+		{
+			for (size_t i = 0; i < 16; i++)
+			{
+				if (val[i] != mat.val[i])
+					return false;
+			}
+			return true;
+		}
+
+		inline bool operator!=(const Matrix4& mat) const
+		{
+			for (size_t i = 0; i < 16; i++)
+			{
+				if (val[i] == mat.val[i])
+					return false;
+			}
+			return true;
+		}
+
+		inline void operator=(const Matrix3& mat)
+		{
+			m[0][0] = mat.m[0][0]; m[0][1] = mat.m[0][1]; m[0][2] = mat.m[0][2];
+			m[1][0] = mat.m[1][0]; m[1][1] = mat.m[1][1]; m[1][2] = mat.m[1][2];
+			m[2][0] = mat.m[2][0]; m[2][1] = mat.m[2][1]; m[2][2] = mat.m[2][2];
+		}
+
+		Matrix4 Transpose(void) const;
+
+		/************************************************************************/
+		/*
+			Translate
+			*/
+		/************************************************************************/
+		inline void SetTranslate(const Vector3& v)
+		{
+			m[0][3] = v.x;
+			m[1][3] = v.y;
+			m[2][3] = v.z;
+		}
+
+		inline void SetTranslate(float32 x, float32 y, float32 z)
+		{
+			m[0][3] = x;
+			m[1][3] = y;
+			m[2][3] = z;
+		}
+
+		inline Vector3 GetTranslate() const
+		{
+			return Vector3(m[0][3], m[1][3], m[2][3]);
+		}
+
+		inline void MakeTranslate(const Vector3& v)
+		{
+			Identity();
+			SetTranslate(v);
+		}
+
+		inline void MakeTranslate(float32 x, float32 y, float32 z)
+		{
+			Identity();
+			SetTranslate(x, y, z);
+		}
+
+		inline static Matrix4 GetTranslate(const Vector3& v)
+		{
+			Matrix4 ret = IDENTITY;
+			ret.m[0][3] = v.x;
+			ret.m[1][3] = v.y;
+			ret.m[2][3] = v.z;
+			return ret;
+		}
+
+		inline static Matrix4 GetTranslate(float32 x, float32 y, float32 z)
+		{
+			Matrix4 ret = IDENTITY;
+			ret.m[0][3] = x;
+			ret.m[1][3] = y;
+			ret.m[2][3] = z;
+			return ret;
+		}
+
+		/************************************************************************/
+		/*
+			Scale
+			*/
+		/************************************************************************/
+		inline void SetScale(const Vector3& v)
+		{
+			m[0][0] = v.x;
+			m[1][1] = v.y;
+			m[2][2] = v.z;
+		}
+
+		inline void SetScale(float32 x, float32 y, float32 z)
+		{
+			m[0][0] = x;
+			m[1][1] = y;
+			m[2][2] = z;
+		}
+
+		inline Vector3 GetScale() const
+		{
+			return Vector3(m[0][0], m[1][1], m[2][2]);
+		}
+
+		inline void MakeScale(const Vector3& v)
+		{
+			Identity();
+			SetScale(v);
+		}
+
+		inline void MakeScale(float32 x, float32 y, float32 z)
+		{
+			Identity();
+			SetScale(x, y, z);
+		}
+
+		inline static Matrix4 GetScale(const Vector3& v)
+		{
+			Matrix4 ret = IDENTITY;
+			ret.m[0][0] = v.x;
+			ret.m[1][1] = v.y;
+			ret.m[2][2] = v.z;
+			return ret;
+		}
+
+		inline static Matrix4 GetScale(float32 x, float32 y, float32 z)
+		{
+			Matrix4 ret = IDENTITY;
+			ret.m[0][0] = x;
+			ret.m[1][1] = y;
+			ret.m[2][2] = z;
+			return ret;
+		}
+
+		inline Matrix3 GetMatrix3() const
+		{
+			Matrix3 ret;
+			ret.m[0][0] = m[0][0];
+			ret.m[0][1] = m[0][1];
+			ret.m[0][2] = m[0][2];
+			ret.m[1][0] = m[1][0];
+			ret.m[1][1] = m[1][1];
+			ret.m[1][2] = m[1][2];
+			ret.m[2][0] = m[2][0];
+			ret.m[2][1] = m[2][1];
+			ret.m[2][2] = m[2][2];
+			return ret;
+		}
+
+		Matrix4 Inverse() const;
+
+		inline bool IsAffine() const
+		{
+			return m[3][0] == 0 && m[3][1] == 0 && m[3][2] == 0 && m[3][3] == 1;
+		}
+
+		Matrix4 InverseAffine() const;
+
+	public:
+		static const Matrix4 ZERO;
+		static const Matrix4 ZEROAFFINE;
+		static const Matrix4 IDENTITY;
+	};
+}
+
+#endif
---
+++ ZFXEngine/src/ZFXMatrix4.cpp
@@ -0,0 +1,138 @@
+#include "ZFXMatrix4.h"
+
+namespace ZFX
+{
+	const Matrix4 Matrix4::ZERO(
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 0, 0, 0);
+
+	const Matrix4 Matrix4::ZEROAFFINE(
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 0, 0, 1);
+
+	const Matrix4 Matrix4::IDENTITY(
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1);
+
+	ZFX::Matrix4 Matrix4::Transpose(void) const
+	{
+		return Matrix4(
+			m[0][0], m[1][0], m[2][0], m[3][0],
+			m[0][1], m[1][1], m[2][1], m[3][1],
+			m[0][2], m[1][2], m[2][2], m[3][2],
+			m[0][3], m[1][3], m[2][3], m[3][3]);
+	}
+
+	ZFX::Matrix4 Matrix4::Inverse() const
+	{
+		float32 m00 = m[0][0], m01 = m[0][1], m02 = m[0][2], m03 = m[0][3];
+		float32 m10 = m[1][0], m11 = m[1][1], m12 = m[1][2], m13 = m[1][3];
+		float32 m20 = m[2][0], m21 = m[2][1], m22 = m[2][2], m23 = m[2][3];
+		float32 m30 = m[3][0], m31 = m[3][1], m32 = m[3][2], m33 = m[3][3];
+
+		float32 v0 = m20 * m31 - m21 * m30;
+		float32 v1 = m20 * m32 - m22 * m30;
+		float32 v2 = m20 * m33 - m23 * m30;
+		float32 v3 = m21 * m32 - m22 * m31;
+		float32 v4 = m21 * m33 - m23 * m31;
+		float32 v5 = m22 * m33 - m23 * m32;
+
+		float32 t00 = +(v5 * m11 - v4 * m12 + v3 * m13);
+		float32 t10 = -(v5 * m10 - v2 * m12 + v1 * m13);
+		float32 t20 = +(v4 * m10 - v2 * m11 + v0 * m13);
+		float32 t30 = -(v3 * m10 - v1 * m11 + v0 * m12);
+
+		float32 invDet = 1 / (t00 * m00 + t10 * m01 + t20 * m02 + t30 * m03);
+
+		float32 d00 = t00 * invDet;
+		float32 d10 = t10 * invDet;
+		float32 d20 = t20 * invDet;
+		float32 d30 = t30 * invDet;
+
+		float32 d01 = -(v5 * m01 - v4 * m02 + v3 * m03) * invDet;
+		float32 d11 = +(v5 * m00 - v2 * m02 + v1 * m03) * invDet;
+		float32 d21 = -(v4 * m00 - v2 * m01 + v0 * m03) * invDet;
+		float32 d31 = +(v3 * m00 - v1 * m01 + v0 * m02) * invDet;
+
+		v0 = m10 * m31 - m11 * m30;
+		v1 = m10 * m32 - m12 * m30;
+		v2 = m10 * m33 - m13 * m30;
+		v3 = m11 * m32 - m12 * m31;
+		v4 = m11 * m33 - m13 * m31;
+		v5 = m12 * m33 - m13 * m32;
+
+		float32 d02 = +(v5 * m01 - v4 * m02 + v3 * m03) * invDet;
+		float32 d12 = -(v5 * m00 - v2 * m02 + v1 * m03) * invDet;
+		float32 d22 = +(v4 * m00 - v2 * m01 + v0 * m03) * invDet;
+		float32 d32 = -(v3 * m00 - v1 * m01 + v0 * m02) * invDet;
+
+		v0 = m21 * m10 - m20 * m11;
+		v1 = m22 * m10 - m20 * m12;
+		v2 = m23 * m10 - m20 * m13;
+		v3 = m22 * m11 - m21 * m12;
+		v4 = m23 * m11 - m21 * m13;
+		v5 = m23 * m12 - m22 * m13;
+
+		float32 d03 = -(v5 * m01 - v4 * m02 + v3 * m03) * invDet;
+		float32 d13 = +(v5 * m00 - v2 * m02 + v1 * m03) * invDet;
+		float32 d23 = -(v4 * m00 - v2 * m01 + v0 * m03) * invDet;
+		float32 d33 = +(v3 * m00 - v1 * m01 + v0 * m02) * invDet;
+
+		return Matrix4(
+			d00, d01, d02, d03,
+			d10, d11, d12, d13,
+			d20, d21, d22, d23,
+			d30, d31, d32, d33);
+	}
+
+	ZFX::Matrix4 Matrix4::InverseAffine() const
+	{
+		assert(IsAffine());
+
+		float32 m10 = m[1][0], m11 = m[1][1], m12 = m[1][2];
+		float32 m20 = m[2][0], m21 = m[2][1], m22 = m[2][2];
+
+		float32 t00 = m22 * m11 - m21 * m12;
+		float32 t10 = m20 * m12 - m22 * m10;
+		float32 t20 = m21 * m10 - m20 * m11;
+
+		float32 m00 = m[0][0], m01 = m[0][1], m02 = m[0][2];
+
+		float32 invDet = 1 / (m00 * t00 + m01 * t10 + m02 * t20);
+
+		t00 *= invDet; t10 *= invDet; t20 *= invDet;
+
+		m00 *= invDet; m01 *= invDet; m02 *= invDet;
+
+		float32 r00 = t00;
+		float32 r01 = m02 * m21 - m01 * m22;
+		float32 r02 = m01 * m12 - m02 * m11;
+
+		float32 r10 = t10;
+		float32 r11 = m00 * m22 - m02 * m20;
+		float32 r12 = m02 * m10 - m00 * m12;
+
+		float32 r20 = t20;
+		float32 r21 = m01 * m20 - m00 * m21;
+		float32 r22 = m00 * m11 - m01 * m10;
+
+		float32 m03 = m[0][3], m13 = m[1][3], m23 = m[2][3];
+
+		float32 r03 = -(r00 * m03 + r01 * m13 + r02 * m23);
+		float32 r13 = -(r10 * m03 + r11 * m13 + r12 * m23);
+		float32 r23 = -(r20 * m03 + r21 * m13 + r22 * m23);
+
+		return Matrix4(
+			r00, r01, r02, r03,
+			r10, r11, r12, r13,
+			r20, r21, r22, r23,
+			0, 0, 0, 1);
+	}
+
+}

736725083ba0d1727e1c5587d60d80a5f3f34a9f
Chris Cheng <gemini2015@hotmail.com>
2015-04-27 15:33:44 +0800

    add zfx.h

---
+++ ZFXEngine/include/ZFX.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ZFXPrerequisites.h"
+
+#include "ZFXColorValue.h"
+#include "ZFXEnum.h"
+#include "ZFXLight.h"
+#include "ZFXLog.h"
+#include "ZFXLogManager.h"
+#include "ZFXMatrix3.h"
+#include "ZFXMatrix4.h"
+#include "ZFXPlane.h"
+#include "ZFXRenderSystem.h"
+#include "ZFXRenderWindow.h"
+#include "ZFXRoot.h"
+#include "ZFXSharedPtr.h"
+#include "ZFXVector3.h"
+#include "ZFXVector4.h"
+#include "ZFXViewport.h"

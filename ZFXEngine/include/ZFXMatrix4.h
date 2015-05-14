c3aa3260ce6cf7cd603edcba931a39a916d4969e
Chris Cheng <gemini2015@hotmail.com>
2015-05-14 21:44:12 +0800

    update matrix4

--- ZFXEngine/include/ZFXMatrix4.h
+++ ZFXEngine/include/ZFXMatrix4.h
@@ -2,6 +2,7 @@
 #define _ZFX_MATRIX_4_H_
 
 #include "ZFXPrerequisites.h"
+#include "ZFXVector3.h"
 #include "ZFXVector4.h"
 #include "ZFXPlane.h"
 #include "ZFXMatrix3.h"
@@ -112,6 +113,18 @@ namespace ZFX
 			ret.m[3][1] = m[3][0] * mat.m[0][1] + m[3][1] * mat.m[1][1] + m[3][2] * mat.m[2][1] + m[3][3] * mat.m[3][1];
 			ret.m[3][2] = m[3][0] * mat.m[0][2] + m[3][1] * mat.m[1][2] + m[3][2] * mat.m[2][2] + m[3][3] * mat.m[3][2];
 			ret.m[3][3] = m[3][0] * mat.m[0][3] + m[3][1] * mat.m[1][3] + m[3][2] * mat.m[2][3] + m[3][3] * mat.m[3][3];
+
+			return ret;
+		}
+
+		inline Vector3 operator * (const Vector3& v) const
+		{
+			Vector3 ret;
+			float32 invW = 1.0f / (m[3][0] * v.x + m[3][1] * v.y + m[3][2] * v.z + m[3][3]);
+
+			ret.x = (m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z + m[0][3]) * invW;
+			ret.y = (m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z + m[1][3]) * invW;
+			ret.z = (m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z + m[2][3]) * invW;
 
 			return ret;
 		}
